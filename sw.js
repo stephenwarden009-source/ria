8158e371ca0e5bc32692b2378bcb8a5c679b4132
stephenwarden009-source <stephenwarden009@gmail.com>
2026-04-07 09:23:05 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,22 +1,20 @@
-const CACHE = 'ria-v4';
-const ASSETS = ['/', '/index.html', '/manifest.json', '/icon-192.png'];
+const CACHE = 'ria-v5'; // Increased version to force an update
+const ASSETS = [
+  './', 
+  'index.html', 
+  'manifest.json', 
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
 
 self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => {
+      console.log('Caching assets...');
+      return c.addAll(ASSETS);
+    })
+  );
   self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
-  );
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request))
-  );
-});
+// ... the rest of your activate and fetch listeners stay the same
